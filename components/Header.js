e27e3806f1ef4ef05c196eb5c42d4a732367df3c
h0uss3m <hossem.benmustapha@gmail.com@users.noreply.github.com>
2021-04-29 17:05:59 +0100

    building header items

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,26 @@
 import Image from 'next/image'
+import HeaderItem from './HeaderItem'
+import {
+  BadgeCheckIcon,
+  CollectionIcon,
+  HomeIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon,
+} from '@heroicons/react/outline'
 const Header = () => {
   return (
-    <header className=''>
-      <h1>this is the header</h1>
+    <header className='flex flex-col sm:flex-row justify-between items-center h-auto'>
+      <div className='flex flex-grow max-w-2xl justify-evenly'>
+        <HeaderItem title='HOME' Icon={HomeIcon} />
+        <HeaderItem title='TRENDING' Icon={LightningBoltIcon} />
+        <HeaderItem title='VERIFIED' Icon={BadgeCheckIcon} />
+        <HeaderItem title='COLLECTIONS' Icon={CollectionIcon} />
+        <HeaderItem title='SEARCH' Icon={SearchIcon} />
+        <HeaderItem title='ACCOUNT' Icon={UserIcon} />
+      </div>
       <Image
+        className='object-contain'
         src='https://press.hulu.com/wp-content/uploads/2020/02/hulu-white.png'
         width={200}
         height={100}
