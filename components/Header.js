38028217b64bd93ca004e4dd951bdb6b405515a7
h0uss3m <hossem.benmustapha@gmail.com@users.noreply.github.com>
2021-04-29 14:29:50 +0100

    add the next config domains

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+const Header = () => {
+  return (
+    <header className=''>
+      <h1>this is the header</h1>
+      <Image src='https://links.papareact.com/ua6' width={200} height={100} />
+    </header>
+  )
+}
+
+export default Header
---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  images: {
+    domains: ['links.papareact.com/ua6', 'image.tmdb'],
+  },
+}
