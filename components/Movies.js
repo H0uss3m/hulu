55cdda1856a7412ff99cf31eb68a3a5bd3894609
h0uss3m <hossem.benmustapha@gmail.com@users.noreply.github.com>
2021-04-30 17:51:41 +0100

    set all screen resposive

--- components/Movies.js
+++ components/Movies.js
@@ -2,7 +2,7 @@ import Thumbnail from './Thumbnail'
 
 const Movies = ({ movies }) => {
   return (
-    <div>
+    <div className='px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl: flex flex-wrap justify-center'>
       {movies.map((movie) => (
         <Thumbnail key={movie.id} movie={movie} />
       ))}
